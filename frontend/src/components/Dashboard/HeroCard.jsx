cca1854798ef49f7c6edb425d7364ae5ec67ef02
thedebanjanchowdhury <chowdhurydebanjan9@gmail.com>
2025-08-04 21:05:47 +0530

    fresh frontend

--- frontend/src/components/Dashboard/HeroCard.jsx
+++
@@ -1,9 +0,0 @@
-import { GlareCard } from "../ui/glare-card";
-
-export function HeroCard() {
-  return (
-    <GlareCard className="flex flex-col items-center justify-center">
-      <p className="text-white font-bold text-xl mt-4">Aceternity</p>
-    </GlareCard>
-  );
-}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { BackgroundBeams } from "./components/ui/background-beams.jsx";
-import { HeroUIProvider } from "@heroui/react";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HeroUIProvider>
-      <App />
-      <BackgroundBeams />
-    </HeroUIProvider>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
